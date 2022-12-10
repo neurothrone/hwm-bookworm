ce3b15d5280fa24872e6c54023b68977f3234853
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-10 06:55:57 +0100

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Zaid Neurothrone on 2022-12-10.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
